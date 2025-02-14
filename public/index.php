f23526dd9393373178f76e9e56ee9039bb805552
ActualLearner <abdellahshafi7@gmail.com>
2025-02-14 08:35:21 +0300

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . 'Core/functions.php';
+
+spl_autoload_register(function ($class) {
+    // Core\Database
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+
+    require base_path("{$class}.php");
+});
+
+$router = new Core\Router();
+
+$routes = require base_path('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$method = $_POST["_method"] ?? $_SERVER["REQUEST_METHOD"];
+
+use Core\App;
+use Core\Database;
+use Core\Container;
+
+
+$cont = new Container();
+$cont->bind('Core\Database', function () {
+    $config = require base_path('config.php');
+    return new Database($config['database']);
+});
+
+App::setContainer($cont);
+$router->route($uri, $method);
